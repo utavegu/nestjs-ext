864e0775ab5a4a9a632eb8bd1ededad98752ba89
Utavegu <utavegu@list.ru>
2023-02-13 13:09:55 +0500

    feat: add books tests

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,18 +1,66 @@
+import { Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksService } from './books.service';
+import { Book, BookDocument } from './schemas/book.schema';
+import {
+  mockBook,
+  mockId,
+  mockCreateBookDto,
+  mockUpdateBookDto,
+} from './mocks/books.mock';
 
-describe('BooksService', () => {
+describe('book service', () => {
   let service: BooksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksService],
+      providers: [
+        BooksService,
+        {
+          provide: getModelToken(Book.name),
+          useValue: Model<BookDocument>,
+        },
+      ],
     }).compile();
 
     service = module.get<BooksService>(BooksService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  test('Добавить книгу в библиотеку', async () => {
+    jest
+      .spyOn(service, 'create')
+      .mockImplementation(async () => mockCreateBookDto);
+    expect(await service.create(mockCreateBookDto)).toEqual(mockCreateBookDto);
+  });
+
+  test('Получить массив всех книг', async () => {
+    jest
+      .spyOn(service, 'findAll')
+      .mockImplementation(async () => [mockBook, mockBook, mockBook]);
+    expect(await service.findAll()).toEqual([mockBook, mockBook, mockBook]);
+  });
+
+  test('Получить целевую книгу', async () => {
+    jest.spyOn(service, 'findOne').mockImplementation(async () => mockBook);
+    expect(await service.findOne(mockId)).toEqual(mockBook);
+  });
+
+  test('Удалить целевую книгу', async () => {
+    jest.spyOn(service, 'delete').mockImplementation(async () => mockBook);
+    expect(await service.delete(mockId)).toEqual(mockBook);
+  });
+
+  test('Редактировать целевую книгу', async () => {
+    jest
+      .spyOn(service, 'update')
+      .mockImplementation(async () => mockUpdateBookDto);
+    expect(await service.update(mockId, mockUpdateBookDto)).toEqual(
+      mockUpdateBookDto,
+    );
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 });
---
+++ src/books/mocks/books.mock.ts
@@ -0,0 +1,23 @@
+// с эни я тут конечно... неплохо бы нормально сделать
+
+const mockBook: any = {
+  title: 'Первая книга',
+  description: 'Ее описание',
+  authors: ['Пушкин', 'Лермонтов'],
+};
+
+const mockCreateBookDto: any = {
+  title: 'Заголовок',
+  description: 'Описание',
+  authors: ['Человек 1', 'Человек 2'],
+};
+
+const mockUpdateBookDto: any = {
+  title: 'Заголовок',
+  description: 'Описание',
+  authors: ['Человек 1', 'Человек 2'],
+};
+
+const mockId = '63ce577c4ae871832103a9c3';
+
+export { mockBook, mockCreateBookDto, mockUpdateBookDto, mockId };
